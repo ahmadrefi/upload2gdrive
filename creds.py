e4af4814fbdc31f2e6808de4a646a1f9fdf3a3a1
ahmadrefi <ahmadrefijuliano@gmail.com>
2020-07-07 06:39:37 +0700

    lanjut

--- creds.py
+++ creds.py
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1-ZJ_b1I3cY03tNO_6zavwSrfywsIPCEk"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AD9mdhGkpoNWUk9PVA"
     
     
     
