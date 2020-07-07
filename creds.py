82004a1f3d84c097735758ffbe97ece444c0f1e0
ahmadrefi <ahmadrefijuliano@gmail.com>
2020-07-07 09:39:59 +0700

    google drive api

--- creds.py
+++ creds.py
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1Gu7uHazFIlgMLNeKx1OlWJVl_PoZHTM2"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0ALRjUZXTCkFlUk9PVA"
     
     
     
