48097430aeb79a21d696f03d943dab6bf1fc01bf
ahmadrefi <ahmadrefijuliano@gmail.com>
2020-07-09 04:34:09 +0700

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -5,8 +5,8 @@ drive_folder_name = "upload2gdrive"
 
 
 # Enter Your Mega email And Pass (Required)
-MEGA_EMAIL = ""
-MEGA_PASSWORD = ""
+MEGA_EMAIL = "ahmadrefi001@gmail.com"
+MEGA_PASSWORD = "kaitokid001"
 
 
 START = " Hi {}  \nI am Drive Uploader Bot . Please Authorise To use me .  "
@@ -28,19 +28,13 @@ DOWNLOAD = "Downloading Started ..."
 DOWN_MEGA = "Downloading Started... \n  Mega Links are \n Extremely Slow "
 DOWN_COMPLETE = "Downloading complete !!"
 NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth  \n\n "
-REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At  "
+REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n   "
 AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link "
-ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n report At  "
+ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n   "
 AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
 UPLOADING = "Download Complete !! \n Uploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
-# DOWN_PATH = "Downloads\\" #windows path
-DOWN_PATH = "Downloads/"  # Linux path
+DOWN_PATH = "Downloads/"  
 DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n report At "
-OPENLOAD = True
-DROPBOX = True
-MEGA = True
-
-
+AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n  "
 UPDATE = """"""
