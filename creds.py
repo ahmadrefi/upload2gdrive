e0780b0bcc22cb4ba260808562cc5870998dd1f9
ahmadrefi <ahmadrefijuliano@gmail.com>
2020-07-09 05:03:49 +0700

    update

--- creds.py
+++ creds.py
@@ -1,12 +1,7 @@
 class Creds():
     # ENTER Your bot Token Here
     TG_TOKEN = "1296711956:AAFieOkYsH-PjAXK_97M6EZZEbgf4Po_p1Q"
-    
-    
-    
-    #  Make Sure You Are Providing both value if you need Teamdrive upload
-    # Because of pydrive And pydrive v2 Api
-    
+
     #Folder Id Of Teamdrive
     TEAMDRIVE_FOLDER_ID = "1Gu7uHazFIlgMLNeKx1OlWJVl_PoZHTM2"
     
--- plugins/wdl.py
+++ plugins/wdl.py
@@ -4,8 +4,6 @@ import subprocess
 def wget_dl(url):
         try:
             print("Downloading Started")
-            # i was facing some problem in filename That's Why i did this ,
-            #  i will fix it later :(
 
             filename = os.path.basename(url)
             output = subprocess.check_output("wget '--output-document' '{}' '{}' ".format(filename , url), stderr=subprocess.STDOUT, shell=True)
