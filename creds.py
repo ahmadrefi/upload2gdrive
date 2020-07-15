7335118c12110f146029578cfc71d55cc92c3a98
ahmadrefi <ahmadrefijuliano@gmail.com>
2020-07-16 05:52:02 +0700

    Update creds.py

--- creds.py
+++ creds.py
@@ -3,8 +3,8 @@ class Creds():
     TG_TOKEN = "1296711956:AAFieOkYsH-PjAXK_97M6EZZEbgf4Po_p1Q"
 
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = "1Gu7uHazFIlgMLNeKx1OlWJVl_PoZHTM2"
+    TEAMDRIVE_FOLDER_ID = ""
     
     # Id of Team drive 
-    TEAMDRIVE_ID = "0ALRjUZXTCkFlUk9PVA"
+    TEAMDRIVE_ID = ""
   
